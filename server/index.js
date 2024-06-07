5ca4c40d24697aee441c24da9b45892c6107a63a
unknown <dhabihullah2@gmail.com>
2024-06-07 17:21:13 +0530

    made changes in server index.js file

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const {noFound,errorHandling} = require('./middleware/error-middleware')
 const app = express()
 app.use(express.json({extended:true}))  
 app.use(express.urlencoded({extended:true}))                  
-app.use(cors({credentials:true,origin:['http://localhost:3000', 'https://main--stalwart-alpaca-9fb951.netlify.app/']}))
+app.use(cors({credentials:true,origin:'*'}))
 app.use(upload())
 app.use('/uploads',express.static(__dirname + '/uploads'))
 
